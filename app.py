b90306ffc3d20a16036f85eacddf97e111e99088
Asharam Khatik <84329079+asharam582@users.noreply.github.com>
2024-03-13 11:28:01 +0000

    added API route for model prediction

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 app = Flask(__name__)
@@ -18,6 +18,14 @@ def predict():
     prediction=clf.predict(vectorised_mail)
     prediction=1 if prediction==1 else -1
     return render_template('index.html',prediction=prediction,email=email)
+@app.route('/api/predict',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    email=data['email-content']
+    vectorised_mail=cv.tranform([email])
+    prediction=clf.predict(vectorised_mail)
 
+    prediction=1 if prediction==1 else -1
+    return jsonify({'prediction':prediction,'email':email})
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True)
